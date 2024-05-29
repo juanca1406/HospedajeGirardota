512c729dfd5d6ff63aee96accb9dfa09fd7bcbb4
juanca1406 <juan1406ma@gmail.com>
2024-05-28 22:09:02 -0500

    v3.3

--- hospedaje/src/Api/Hospedaje.tsx
+++
@@ -1,9 +0,0 @@
-const fetchUsers = async () => {
-    const response = await fetch('https://localhost:7288/api/Hospedajes/');
-    if (!response.ok) {
-        throw new Error('La respuesta de la red no estuvo bien');
-    }
-    return response.json();
-};
-
-export { fetchUsers };
